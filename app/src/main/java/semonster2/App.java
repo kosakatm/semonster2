3ef53b89c82e9a9a9f6845d3a61d1864201e60bc
Hiyuu Suzuki <m1m21a26@oit.ac.jp>
2021-07-19 10:39:15 +0900

    関数を使えるようにしました！

--- app/src/main/java/semonster2/App.java
+++ app/src/main/java/semonster2/App.java
@@ -3,12 +3,34 @@
  */
 package semonster2;
 
+import java.util.LinkedList;
+import java.util.Random;
+
 public class App {
+    final static Random random = new Random();
+    final static int maxRandomNumber = 4;// ランダムな数値の最大値(0～4)
     public String getGreeting() {
         return "こんにちは SEMonster";
     }
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+
     }
+    /**
+   * 指定した数の0~4(maxRandomNumber)のランダムな数値を作成し，LinkedListに格納して返却する
+   *
+   * @param randNum
+   * @return
+   */
+  public static LinkedList<Integer> createRandomList(int count) {
+    LinkedList<Integer> randNumList = new LinkedList<>();
+    for (int i = 0; i < count; i++) {
+      randNumList.add(random.nextInt(maxRandomNumber + 1));
+    }
+
+    return randNumList;
+  }
+
+
 }
