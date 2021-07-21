aa76cfe3bdb6d2a6a773f4e6a681a11c13ef3a0e
kosakatm <m1m21a19@st.oit.ac.jp>
2021-07-22 08:58:42 +0900

    標準入力で自分の名前を入力できるようにしました

--- app/src/main/java/semonster2/Monster.java
+++ app/src/main/java/semonster2/Monster.java
@@ -1,17 +1,13 @@
 package semonster2;
-import java.util.Random;
 
 public class Monster {
   private String name;
   private int rare; // 戦うとレア度が高いほうが勝つ．同じ場合は引き分け
 
-  Monster() {
-    Random random = new Random();
-    this.name = this.summonMonster(random.nextInt(5));
-    this.rare = random.nextInt(5);
+  Monster(int nameNum, int rareNum) {
+    this.name = this.summonMonster(nameNum);
+    this.rare = rareNum;
   }
-
-
 
   String summonMonster(int mnumber) {
     String monsters[] = { "スライム", "サハギン", "ドラゴン", "デュラハン", "シーサーペント" };
